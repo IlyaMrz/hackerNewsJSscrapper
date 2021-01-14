b9e39b608bf678134f6b9ea92b932ded9a4b4d3a
morozov ilya <knpkaa@gmail.com>
2021-01-14 20:54:15 +0300

    server implementing

---
+++ hackerNewsAPI/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bP = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'sDAWCs1Z',
+        database: 'hackernews'
+    }
+});
+
+const app = express();
+
+app.use(bP.json());
+app.use(cors());
+
+app.get('/', (req,res) =>{
+    db.select('*').from('news')
+    .then(i => {
+        res.json(i)
+    })
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000.')
+})
