90ceb0a164b44e02e78b92e29f6780f5ba2e9a35
morozov ilya <knpkaa@gmail.com>
2021-01-14 20:56:55 +0300

    ps del

--- hackerNewsAPI/server.js
+++ hackerNewsAPI/server.js
@@ -9,7 +9,7 @@ const db = knex({
     connection: {
         host: '127.0.0.1',
         user: 'postgres',
-        password: 'sDAWCs1Z',
+        password: '',
         database: 'hackernews'
     }
 });
