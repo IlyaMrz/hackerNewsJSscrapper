d44d394eea0f8f407c95cecb5aa2891f641eb9b9
morozov ilya <knpkaa@gmail.com>
2021-01-15 22:19:30 +0300

    rebuild componentDidMount needs work on list comparing

--- hackernewsfe/src/App.js
+++ hackernewsfe/src/App.js
@@ -8,42 +8,79 @@ constructor() {
     super()
     this.state = {
         stories: [],
+        storiesRaw: [],
         storiesDB: [],
         stroiesToDisplay: []
     }
 }
+
+// componentDidMount() {
+//     fetch(`https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty`)
+//         .then(response => response.json())
+//         .then(data => {
+//             const data2 = data.slice(0,40); // comment for data
+//             data2.forEach(element => { //data2 for slicing. data for full
+//                 fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json`)
+//                 .then(response => response.json())
+//                 .then(result => {
+//                     var joined = this.state.storiesRaw.concat(result);
+//                     this.setState({storiesRaw:joined})
+//                     // console.log(result.length)    
+//                 })
+//             })
+//             fetch('http://localhost:3000/')
+//                         .then(response => response.json())
+//                         .then(data =>{
+//                             data.forEach(elem => {
+//                                 console.log(elem.id);
+//                                 const loadedStories = this.state.storiesRaw;
+//                                 const storiesWithoutDB = loadedStories.filter( item => item.id !== elem.id);
+//                                 this.setState({stories:storiesWithoutDB})
+//                             })
+//                         })
+//             // 
+//             // const storiesWithoutDB = loadedStories.filter( item => item.id != elem.id)
+            
+//         }
+//     );
+    
+// }
 
 componentDidMount() {
     fetch(`https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty`)
         .then(response => response.json())
         .then(data => {
             const data2 = data.slice(0,40); // comment for data
-            data2.forEach(element => { //data2 for slicing. data for full
-                fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json`)
-                .then(response => response.json())
-                .then(result => {
-                    var joined = this.state.stories.concat(result);
-                    this.setState({stories:joined})
-                    // console.log(result)    
-                })
+            this.setState({storiesRaw:data2})
             })
             fetch('http://localhost:3000/')
-                        .then(response => response.json())
-                        .then(data =>{
-                            data.forEach(elem => {
-                                console.log(elem.id);
-                                const loadedStories = this.state.stories;
-                                const storiesWithoutDB = loadedStories.filter( item => item.id != elem.id);
-                                this.setState({stories:storiesWithoutDB})
+                .then(response => response.json())
+                .then(data =>{
+                    data.forEach(element => {
+                        var eleID = this.state.storiesDB.concat(element.id);
+                        this.setState({storiesDB:eleID});
+                    });
+                    const array1 = this.state.storiesRaw;
+                    console.log(`1 ${array1}`);
+                    const array2 = this.state.storiesDB;
+                    console.log(`2 ${array2}`);
+                    const array3 = array1.filter(val => !array2.includes(val)); // TO DO: wrong arrays comparing
+                    console.log(`array3 ${array3}`);
+                    array3.forEach(element =>{
+                            console.log(`for each3rd ${element}`);
+                            fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json`)
+                            .then(response => response.json())
+                            .then(result => {
+                                var joined = this.state.stories.concat(result);
+                                this.setState({stories:joined})
+                                // console.log(result.length)    
                             })
-                        })
-            // 
-            // const storiesWithoutDB = loadedStories.filter( item => item.id != elem.id)
-            
+                    })
         }
     );
-    
 }
+
+
 
 onSubmitArchive = (id, event) => {
     const currentStories = this.state.stories;
