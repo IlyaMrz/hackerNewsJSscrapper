a1f343a0571a445c644152cf988080f7391257e1
morozov ilya <knpkaa@gmail.com>
2021-05-11 15:41:48 +0300

    a bit less ugly on mobile view. css adj.

--- hackernews_hooks_Firestore/src/components/Hat.js
+++ hackernews_hooks_Firestore/src/components/Hat.js
@@ -3,8 +3,10 @@ import { auth } from '../firebase/firebase.utils';
 
 const Hat = () =>(
             <div style={{"minWidth":"90vw"}}>
-                <div className="fl w-80 pa2"></div>
-                <div className="fl w-20 pa2" >
+                <div className="fl w-70 pa2">
+                    <h1 className="ma0 ml3-ns f4 f2-ns">Best hacker news!</h1>
+                </div>
+                <div className="fl w-30 pa2" >
                     <a href='/#' style={{'whiteSpace':'nowrap'}} className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green grow shadow-3" 
                     onClick={()=>auth.signOut()} > Log Out</a>
                 </div>
