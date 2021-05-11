9f01dcade35e278271828a2508e62303434fdc7f
morozov ilya <knpkaa@gmail.com>
2021-05-11 21:39:04 +0300

    hat's css adjustments

--- hackernews_hooks_Firestore/src/components/Hat.js
+++ hackernews_hooks_Firestore/src/components/Hat.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { auth } from '../firebase/firebase.utils';
 
 const Hat = () =>(
-            <div style={{"minWidth":"90vw"}}>
-                <div className="fl w-70 pa2">
-                    <h1 className="ma0 ml3-ns f4 f2-ns">Best hacker news!</h1>
-                </div>
-                <div className="fl w-30 pa2" >
-                    <a href='/#' style={{'whiteSpace':'nowrap'}} className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green grow shadow-3" 
-                    onClick={()=>auth.signOut()} > Log Out</a>
-                </div>
+            <div className="w-60-ns-l-m mw98 flex flex-row justify-between">
+                    <div className="flex-auto ml5-ns">
+                        <h1 className="f3 f2-ns pa0">Best hacker news!</h1>
+                    </div>
+                    <div style={{'marginTop':'0.6rem'}}>
+                        <a href='/#' style={{'whiteSpace':'nowrap'}} className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green grow shadow-3" 
+                        onClick={()=>auth.signOut()} > Log Out</a>
+                    </div>
             </div>
         );
 
